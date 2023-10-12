f755d05a8b5fca81543a8801b8520bd3ec575da2
niqeru <147090354+niqeru@users.noreply.github.com>
2023-10-13 01:40:51 +0700

    Update agnez.js

--- js/agnez.js
+++ js/agnez.js
@@ -29,3 +29,78 @@ function ScrollUp() {
 let box1 = document.getElementsByClassName('col-md-5').length;
 
 console.log(box1);
+
+
+
+
+// mod1
+document.getElementById("open_mod_1").addEventListener("click", function() {
+    document.getElementById("mod_1").classList.add("open")
+})
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("mod_1").classList.remove("open")
+    }
+});
+
+document.querySelector("#mod_1 .modal_box").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+document.getElementById("mod_1").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+}); 
+
+document.getElementById("close-my-modal-btn1").addEventListener("click", function() {
+    document.getElementById("mod_1").classList.remove("open")
+})
+
+
+
+// mod2
+document.getElementById("open_mod_2").addEventListener("click", function() {
+    document.getElementById("mod_2").classList.add("open")
+})
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("mod_2").classList.remove("open")
+    }
+});
+
+document.querySelector("#mod_2 .modal_box").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+document.getElementById("mod_2").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+}); 
+
+document.getElementById("close-my-modal-btn2").addEventListener("click", function() {
+    document.getElementById("mod_2").classList.remove("open")
+})
+
+
+// mod3
+document.getElementById("open_mod_3").addEventListener("click", function() {
+    document.getElementById("mod_3").classList.add("open")
+})
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("mod_3").classList.remove("open")
+    }
+});
+
+document.querySelector("#mod_3 .modal_box").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+document.getElementById("mod_3").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+}); 
+
+document.getElementById("close-my-modal-btn3").addEventListener("click", function() {
+    document.getElementById("mod_3").classList.remove("open")
+})
