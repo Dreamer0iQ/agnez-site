3a117f0822ab81e45084020b54531a091f10a551
dreamer <artemkamix1@yandex.ru>
2023-10-13 21:44:34 +0700

    Optimizatin
    Code became easier + cant scroll while intro

--- js/agnez.js
+++ js/agnez.js
@@ -1,24 +1,18 @@
-document.querySelector('#agnez').onclick=function TinyAgnez() {
-    console.log('Шапка уменьшена');
-    const tinyagnez = document.querySelector('#header');
-    tinyagnez.style.height = '13%'
-    const tinyagnez1 = document.querySelector('#headerfikt');
-    tinyagnez1.style.height = '13%'
-}
-
-window.onload = function(){
-
-    setTimeout(loadAfterTime, 1000)
-};
+const body = document.getElementsByTagName('body')[0]
+body.style.overflow = 'hidden'
 
 function loadAfterTime() { 
     console.log('Шапка уменьшена');
+	body.style.overflow = 'scroll'
     const tinyagnez = document.querySelector('#header');
     tinyagnez.style.height = '13%'
-    const tinyagnez1 = document.querySelector('#headerfikt');
-    tinyagnez1.style.height = '13%'
-    }
-    
+}
+
+document.querySelector('#agnez').onclick=loadAfterTime
+
+window.onload = function(){
+    setTimeout(loadAfterTime, 1000)
+};
 
 function ScrollUp() {
     const elem = document.querySelector('#fsf')
@@ -28,79 +22,27 @@ function ScrollUp() {
 
 let box1 = document.getElementsByClassName('col-md-5').length;
 
-console.log(box1);
+const stock = document.getElementById('InStock')
+for (let i =1; i < stock.childNodes.length+1; i++){
+	document.getElementById(`open_mod_${i}`).addEventListener("click", function() {
+		document.getElementById(`mod_${i}`).classList.add("open")
+	})
 
+	window.addEventListener('keydown', (e) => {
+		if (e.key === "Escape") {
+			document.getElementById(`mod_${i}`).classList.remove("open")
+		}
+	});
+	document.querySelector(`#mod_${i} .modal_box`).addEventListener('click', event => {
+		event._isClickWithInModal = true;
+	});
+	document.getElementById(`mod_${i}`).addEventListener('click', event => {
+		if (event._isClickWithInModal) return;
+		event.currentTarget.classList.remove('open');
+	}); 
 
+	document.getElementById(`close-my-modal-btn${i}`).addEventListener("click", function() {
+		document.getElementById(`mod_${i}`).classList.remove("open")
+	})	
+}
 
-
-// mod1
-document.getElementById("open_mod_1").addEventListener("click", function() {
-    document.getElementById("mod_1").classList.add("open")
-})
-
-window.addEventListener('keydown', (e) => {
-    if (e.key === "Escape") {
-        document.getElementById("mod_1").classList.remove("open")
-    }
-});
-
-document.querySelector("#mod_1 .modal_box").addEventListener('click', event => {
-    event._isClickWithInModal = true;
-});
-document.getElementById("mod_1").addEventListener('click', event => {
-    if (event._isClickWithInModal) return;
-    event.currentTarget.classList.remove('open');
-}); 
-
-document.getElementById("close-my-modal-btn1").addEventListener("click", function() {
-    document.getElementById("mod_1").classList.remove("open")
-})
-
-
-
-// mod2
-document.getElementById("open_mod_2").addEventListener("click", function() {
-    document.getElementById("mod_2").classList.add("open")
-})
-
-window.addEventListener('keydown', (e) => {
-    if (e.key === "Escape") {
-        document.getElementById("mod_2").classList.remove("open")
-    }
-});
-
-document.querySelector("#mod_2 .modal_box").addEventListener('click', event => {
-    event._isClickWithInModal = true;
-});
-document.getElementById("mod_2").addEventListener('click', event => {
-    if (event._isClickWithInModal) return;
-    event.currentTarget.classList.remove('open');
-}); 
-
-document.getElementById("close-my-modal-btn2").addEventListener("click", function() {
-    document.getElementById("mod_2").classList.remove("open")
-})
-
-
-// mod3
-document.getElementById("open_mod_3").addEventListener("click", function() {
-    document.getElementById("mod_3").classList.add("open")
-})
-
-window.addEventListener('keydown', (e) => {
-    if (e.key === "Escape") {
-        document.getElementById("mod_3").classList.remove("open")
-    }
-});
-
-document.querySelector("#mod_3 .modal_box").addEventListener('click', event => {
-    event._isClickWithInModal = true;
-});
-document.getElementById("mod_3").addEventListener('click', event => {
-    if (event._isClickWithInModal) return;
-    event.currentTarget.classList.remove('open');
-}); 
-
-document.getElementById("close-my-modal-btn3").addEventListener("click", function() {
-    document.getElementById("mod_3").classList.remove("open")
-})
